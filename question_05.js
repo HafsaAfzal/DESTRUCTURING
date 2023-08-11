3706c03233f8e400dcd09750717a87899d9fe269
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 12:04:57 +0500

    initiall commit

---
+++ question_05.js
@@ -0,0 +1,3 @@
+// Write a function that takes an array of strings and returns an object with two properties:
+// first, which is the first element of the array, and last, which is the last element of the
+// array. Use array destructuring and shorthand property names to create the object.
