0c32b771b7dc567f85b74bc95e16657c70c9f35a
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 11:54:45 +0500

    changes to function code assign default value with correct syntax

--- question_04.js
+++ question_04.js
@@ -2,12 +2,12 @@
 // properties and returns a string with the person’s initials. Use object destructuring with
 // default values to assign the properties to variables.
 const initialls=(obj)=>{
-    let {firstName,middleName:a,lastName}=obj;
-    return `${firstName.charAt(0)}${middleName.charAt(0)}${lastName.charAt(0)}}`
+    let {firstName,middleName="A",lastName}=obj;
+    return `${firstName.charAt(0)}${middleName.charAt(0)}${lastName.charAt(0)}`
 }
 let obj={
     firstName:"Hafsa" ,
+    middleName:"" ,
     lastName:"Afzal",
-    middleName:"" 
 }
 console.log(initialls(obj));
