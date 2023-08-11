6c82e0a4c3b31d0f4f3fba60d4d8c3e6496778c9
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 11:52:05 +0500

    function code added

--- question_04.js
+++ question_04.js
@@ -1,3 +1,13 @@
 // Write a function that takes an object with firstName, lastName and middleName
 // properties and returns a string with the person’s initials. Use object destructuring with
 // default values to assign the properties to variables.
+const initialls=(obj)=>{
+    let {firstName,middleName:a,lastName}=obj;
+    return `${firstName.charAt(0)}${middleName.charAt(0)}${lastName.charAt(0)}}`
+}
+let obj={
+    firstName:"Hafsa" ,
+    lastName:"Afzal",
+    middleName:"" 
+}
+console.log(initialls(obj));
