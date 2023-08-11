1a77935f772e5f5719c20c14c59556666d3c0aa6
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 10:15:32 +0500

    adding two numbers function code added

--- question_01.js
+++ question_01.js
@@ -1,2 +1,10 @@
 // Write a function that takes an array of two numbers and returns their sum. Use array
 // destructuring to assign the numbers to variables.
+const sum=(arr)=>{
+    let [num1 , num2]=arr;
+    let add=num1+num2;
+    return add
+}
+let arr =[2,3];
+let result =sum(arr);
+console.log(result);
