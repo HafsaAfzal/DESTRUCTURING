34226e0184e9f25e8d980325aa92b856ab529d46
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 11:36:42 +0500

    initiall commit

---
+++ question_04.js
@@ -0,0 +1,3 @@
+// Write a function that takes an object with firstName, lastName and middleName
+// properties and returns a string with the person’s initials. Use object destructuring with
+// default values to assign the properties to variables.
