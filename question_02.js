3ad30bd4b7852e0b71c63043246f7e0d822f3c68
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 10:17:28 +0500

    initiall commit

---
+++ question_02.js
@@ -0,0 +1,3 @@
+// Write a function that takes an object with name, age and occupation properties and
+// returns a string with the person’s information. Use object destructuring to assign the
+// properties to variables.
