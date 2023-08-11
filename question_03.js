fd852a67979ee592616bc88b2a3a430c37d2b7ed
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 10:39:10 +0500

    initiall commit

---
+++ question_03.js
@@ -0,0 +1,3 @@
+// Write a function that takes an array of objects with name and score properties and
+// returns the name of the person with the highest score. Use array destructuring and the
+// spread operator (...) to get the first element and the rest of the array.
