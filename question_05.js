2dbc2758cc9971de2abe719ac042b6a403a5b29c
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 12:24:21 +0500

    correct and complete code

--- question_05.js
+++ question_05.js
@@ -1,3 +1,15 @@
 // Write a function that takes an array of strings and returns an object with two properties:
 // first, which is the first element of the array, and last, which is the last element of the
 // array. Use array destructuring and shorthand property names to create the object.
+const generateObj=(str)=>{
+    let[name,...rest]=str;
+    // console.log(str1);
+    let age=rest.length-1;
+    obj={
+       name,
+       age
+    }
+    return obj
+}
+let str=["Hafsa","Aaman",22,20];
+console.log(generateObj(str));
