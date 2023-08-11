571d9712adf8edc8243b89e3b492938a88859dff
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 10:24:29 +0500

    function added

--- question_02.js
+++ question_02.js
@@ -1,3 +1,13 @@
 // Write a function that takes an object with name, age and occupation properties and
 // returns a string with the person’s information. Use object destructuring to assign the
 // properties to variables.
+const info=(person)=>{
+    let {n , a , o}=person;
+    return `My name is ${n} . I am ${a} years old and my occupation is ${o}`
+}
+let person={
+    name:"Hafsa",
+    age:20,
+    occupation:"Software Engineer"
+}
+console.log(info(person));
